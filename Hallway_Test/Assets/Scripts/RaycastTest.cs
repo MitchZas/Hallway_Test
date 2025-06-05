926e3fea5dba1d342eb5b427aeaf22ad01c89e61
MitchZas <mitchellzasa@gmail.com>
2025-06-05 08:04:51 -0400

    Implemented first pass Raycast
    Still need to do tweaks such as distance but raycast is in

---
+++ Hallway_Test/Assets/Scripts/RaycastTest.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class RaycastTest : MonoBehaviour
+{
+    Ray ray;
+    void Start()
+    {
+        
+       
+    }
+
+    void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        ColliderCheck();
+    }
+
+    void ColliderCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+            }
+        }
+    }
+}
