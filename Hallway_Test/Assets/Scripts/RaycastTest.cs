62fbe23329ec340d4b9e763c7866712d4f8b560b
MitchZas <mitchellzasa@gmail.com>
2025-06-07 19:26:16 -0400

    Added a placeholder door and have the key unlock it

--- Hallway_Test/Assets/Scripts/RaycastTest.cs
+++ Hallway_Test/Assets/Scripts/RaycastTest.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 public class RaycastTest : MonoBehaviour
 {
     Ray ray;
-    void Start()
-    {
-        
-       
-    }
+
+    bool BallTaken = false;
+
+    [SerializeField] GameObject lockedDoor;
+    [SerializeField] GameObject Key;
 
     void Update()
     {
@@ -22,8 +22,28 @@ public class RaycastTest : MonoBehaviour
         {
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Debug.Log(hit.collider.gameObject.tag);
-                Destroy(hit.collider.gameObject);
+                if (hit.collider.gameObject.tag == "Ball")
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    BallTaken = true;
+                }
+                
+                if (hit.collider.gameObject.tag == "LockedDoor")
+                {
+                    Debug.Log("Door is Locked");
+                }
+
+                if (hit.collider.gameObject.tag == "LockedDoor" && BallTaken)
+                {
+                    Debug.Log("Door is Unlocked");
+                    lockedDoor.gameObject.SetActive(false);
+                }
+                    // Get the name of the Game Object 
+                    // Pop up UI with that Game Object Name with a prompt to pick up
+                    // Once prompt is hit, "Destroy" game object in scene and place in inventory
+
+                    Debug.Log(hit.collider.gameObject.tag);
+                
             }
         }
     }
